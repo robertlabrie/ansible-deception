bfc96ef68c10fe2bf0bb27e1f57d1ac3f87528b1
Robert Labrie <rolabrie@cisco.com>
2017-11-07 18:31:49 -0500

    a comical amount of WIP

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# ansible-deception
+# The Ansible Deception
+
+Is Ansbile purely declarative? I kept hearing about it's virtues, but it didn't make sense to me how something with loops, conditions, variable assignment and substitution, input/output -- how that could be "purely declarative" the way Kubernetes or Terraform is. I heard a few dissenting voices, which described it more like "programming in YAML". So I thought: "can you write an application in Ansible". The answer is: YES.
+
+Ansible is not a purely declarative configuration management tool, it's an ad-hoc task runner in a category I call "barely imperative". To hit that point harder, I literally wrote a web app using Ansible as a general purpose language. If you're asking "why would you do that" -- instead ask yourself why you would do anything in Ansible: a terrible ad-hoc GPL implemented in YAML?
+
+The `deception.rb` file is a Sinatra wrapper, which accepts HTTP requests, JSONifies them for Ansible, then passes the results back on to the UA. There is *zero application logic* in Ruby here, 100% of the app is written in Ansible.
+
+The simple app presents an upload form where you upload an image. Uploaded images are displayed on the index.
+
+`main.yml` is the router, it calls different routes based on METHOD and PATH.
+
+There is no documentation, because, why not, LOL.
+
+If you try this, you'll need to edit the `datadir` attribute in `deception.rb`
+## To-Do:
+* Get the description feature working
+* Write a session manager
+* Store data in Redis (shell out to redis-cli, why not?)
+* Tabular results (should be no problem with Jinja)
+* Spinning images?
--- deception.rb
+++ deception.rb
@@ -2,28 +2,36 @@
 require 'sinatra'
 require 'sinatra/multi_route'
 require 'json'
-get '/*' do
-#route :get, :post, '/*' do
+route :get, :post, '/*' do
   # puts "#{request.to_yaml}"
   payload = {}
   payload['env'] = request.env
   payload['params'] = request.params
   payload['headerfile'] = '/tmp/header'
   payload['bodyfile'] = '/tmp/body'
+  payload['datadir'] = '/Users/robertlabrie/Documents/tmp/deception'
+
+  # handle uploads here
+  if request.env['REQUEST_METHOD']
+    if params[:file]
+      tempfile = params[:file][:tempfile]
+      FileUtils.cp(tempfile.path, "/tmp/uploaded")
+      payload['tempfile'] = "/tmp/uploaded"
+    end
+  end
   puts "#{payload.to_json}"
-  # request.env.REQUEST_PATH == '/upload'
-  # request.env.REQUEST_METHOD == 'POST'
-  # `ansible-playbook ansible/main.yml -i local, --connection=local -e '{"headerfile":"/tmp/header","bodyfile":"/tmp/body","REQUEST_PATH":"/foo.txt"}'`
   ansible = `ansible-playbook ansible/main.yml -i local, --connection=local -e '#{payload.to_json}'`
   puts "#{ansible}"
+
+  data = File.read(payload['headerfile'])
+  data = JSON.parse(data)
+  http_response = data.select { |key, value| key.to_s.match(/^http_response/) }
+  http_response.each do |k,v|
+    next if k == 'http_response_status'
+    n = k.gsub('http_response_','').gsub('__','-')
+    headers[n] = v
+  end
+  status http_response['http_response_status'] if http_response['http_response_status']
   s = File.binread(payload['bodyfile'])
   s
-end
-
-post '/upload' do
-  if params[:file]
-    tempfile = params[:file][:tempfile]
-    FileUtils.cp(tempfile.path, "/tmp/uploaded")
-  end
-  "the upload the upload what what the upload"
 end
